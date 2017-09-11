68456140a9423b5e189edda08d20fd3e78eb5e10
cjwsstrm <cjwesstrom@gmail.com>
2017-09-11 23:15:25 +0000

    made changes to request_pratice.js

---
+++ request_practice.js
@@ -0,0 +1,21 @@
+var request = require('request');
+var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')               // Note 1
+       .on('error', function (err) {                                   // Note 2
+         throw err;
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ', response.statusCode);
+         console.log('Content Type: ', response.headers['content-type'])
+       })
+       .pipe(fs.createWriteStream('./future.jpg'))               // Note 4
+
+       .on('finish', () => {
+         console.log('Download complete.');
+       });
+// Notes:
+// 1. `request.get` is equivalent to `request()`
+// 2. `request.on('error', callback)` handles any error
+// 3. `request.on('response, callback)` handles the response
+// 4. What is happening here?
